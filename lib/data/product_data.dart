7308332bc067605bdf69b69c212920fe6fd14089
EDGAR_EMILIO_MERAZ_ACOSTA <a.21308051280388@cbtis128.edu.mx>
2024-04-24 15:33:06 +0000

    actualizado

---
+++ lib/data/product_data.dart
@@ -0,0 +1,19 @@
+import 'package:meraz/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/p3r.jpg",
+      title: "Persona 3 Reload",
+      description:
+          "Adentrate en el Tartaro de noche y descubre todo sus misterios"),
+  ProductModel(
+      img: "assets/images/gow.jpg",
+      title: "God Of War Ragnarok",
+      description:
+          "Explora junto a Kratos su nueva mision en la vida y libra batallas Epicas!"),
+  ProductModel(
+      img: "assets/images/spiderman.jpg",
+      title: "Spiderman 2",
+      description:
+          "Balanceate por la ciudad de New York mientras detienes el crimen"),
+];
